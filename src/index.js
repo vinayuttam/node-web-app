fbe9b5b20aa0b4068d1523c4c20dd448a92be7d9
Vinay Uttam Vemparala <15381417+vinayuttam@users.noreply.github.com>
2022-02-22 15:14:30 +0530

    Use npm instead of yarn; Added knex for checking DB connectivity

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.json({ message: "Hello! I'm deployed via Terraform AWS Code Pipeline" });
@@ -8,4 +8,22 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`ExpressJS application listening on port ${port}`);
+});
+
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host : process.env.DB_HOST,
+    port : 5432,
+    user : process.env.DB_USERNAME,
+    password : process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    searchPath: ['public']
+  },
+});
+
+knex.select().table('task').then(tasks => {
+  console.log(tasks);
+}).catch(err => {
+  console.log(err);
 });
