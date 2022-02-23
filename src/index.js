cd208be8852dad919b0128ad7ab2088a702cd655
Vinay Uttam Vemparala <15381417+vinayuttam@users.noreply.github.com>
2022-02-24 01:28:23 +0530

    Add sample routes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.json({ message: "Hello! I'm deployed via Terraform AWS Code Pipeline" });
-});
-
-app.listen(port, () => {
-  console.log(`ExpressJS application listening on port ${port}`);
-});
-
 const knex = require('knex')({
   client: 'pg',
   connection: {
@@ -22,8 +14,42 @@ const knex = require('knex')({
   },
 });
 
-knex.select().table('task').then(tasks => {
-  console.log(tasks);
-}).catch(err => {
-  console.log(err);
+
+app.get('/', (req, res) => {
+  res.json({ message: "Hello! I'm deployed via Terraform AWS Code Pipeline" });
 });
+
+app.get('/create-table', (req, res) => {
+  knex.schema.hasTable('task').then(function (exists) {
+    if (!exists) {
+      knex.schema.createTable('task', function (table) {
+        table.increments('id').primary();
+        table.string('title');
+        table.timestamps();
+      }).then(result => {
+        res.json({ message: 'Table created!' });
+      }).catch(err => {
+        console.log(err.message);
+      })
+    } else {
+      res.json({ message: 'Table already exists!' });
+    }
+  });
+});
+
+app.get('/tasks/insert', async (req, res) => {
+  const task = await knex('task').insert({ title: 'Slaughterhouse Five' });
+  res.json({ task });
+});
+
+app.get('/tasks', async (req, res) => {
+  const tasks = await knex.select().table('task');
+  res.json({ tasks });
+});
+
+app.listen(port, () => {
+  console.log(`ExpressJS application listening on port ${port}`);
+});
+
+
+
